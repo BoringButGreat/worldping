e2ee1ad908ed1426032f2d34348941c99b7eedfc
Peter B <peter.bonney@gmail.com>
2016-06-23 16:11:10 -0400

    fixes arity of monitor function defs

--- lib/monitor/monitor.ex
+++ lib/monitor/monitor.ex
@@ -5,7 +5,7 @@ defmodule Worldping.Monitor do
   Create a JSON specification for an http monitor based on the given parameters
   """
   def http(host \\ "www.raintank.io", port \\ "80", path \\ "/", timeout \\ "5", method \\ "GET", expectRegex \\ "", endpoint_id \\ -1, collector_ids \\ [1, 2, 3, 4], collector_tags \\ [], enabled \\ true, frequency \\ 120, health_steps \\ 3, health_collectors \\ 3, health_notification_enabled \\ true, health_notification_email \\ "")
-  def http(host, port, path, timeout, method, expectRegex, collector_ids, collector_tags, enabled, frequency, health_steps, health_collectors, health_notification_enabled, health_notification_email) do
+  def http(host, port, path, timeout, method, expectRegex, endpoint_id, collector_ids, collector_tags, enabled, frequency, health_steps, health_collectors, health_notification_enabled, health_notification_email) do
     settings = [
       %{variable: "host", value: host},
       %{variable: "port", value: port},
@@ -22,7 +22,7 @@ defmodule Worldping.Monitor do
   Create a JSON specification for an https monitor based on the given parameters
   """
   def https(host \\ "www.raintank.io", port \\ "443", path \\ "/", timeout \\ "5", method \\ "GET", expectRegex \\ "", validateCert \\ true, endpoint_id \\ -1, collector_ids \\ [1, 2, 3, 4], collector_tags \\ [], enabled \\ true, frequency \\ 120, health_steps \\ 3, health_collectors \\ 3, health_notification_enabled \\ true, health_notification_email \\ "")
-  def https(host, port, path, timeout, method, expectRegex, validateCert, collector_ids, collector_tags, enabled, frequency, health_steps, health_collectors, health_notification_enabled, health_notification_email) do
+  def https(host, port, path, timeout, method, expectRegex, validateCert, endpoint_id, collector_ids, collector_tags, enabled, frequency, health_steps, health_collectors, health_notification_enabled, health_notification_email) do
     settings = [
       %{variable: "host", value: host},
       %{variable: "port", value: port},
@@ -40,7 +40,7 @@ defmodule Worldping.Monitor do
   Create a JSON specification for a ping monitor based on the given parameters
   """
   def ping(hostname \\ "www.raintank.io", timeout \\ "5", endpoint_id \\ -1, collector_ids \\ [1, 2, 3, 4], collector_tags \\ [], enabled \\ true, frequency \\ 120, health_steps \\ 3, health_collectors \\ 3, health_notification_enabled \\ true, health_notification_email \\ "")
-  def ping(hostname, timeout, collector_ids, collector_tags, enabled, frequency, health_steps, health_collectors, health_notification_enabled, health_notification_email) do
+  def ping(hostname, timeout, endpoint_id, collector_ids, collector_tags, enabled, frequency, health_steps, health_collectors, health_notification_enabled, health_notification_email) do
     settings = [
       %{variable: "hostname", value: hostname},
       %{variable: "timeout", value: timeout}
@@ -53,7 +53,7 @@ defmodule Worldping.Monitor do
   Create a JSON specification for a DNS monitor based on the given parameters
   """
   def dns(name \\ "raintank.io", type \\ "A", protocol \\ "udp", timeout \\ "5", endpoint_id \\ -1, collector_ids \\ [1, 2, 3, 4], collector_tags \\ [], enabled \\ true, frequency \\ 120, health_steps \\ 3, health_collectors \\ 3, health_notification_enabled \\ true, health_notification_email \\ "")
-  def dns(name, type, protocol, timeout, collector_ids, collector_tags, enabled, frequency, health_steps, health_collectors, health_notification_enabled, health_notification_email) do
+  def dns(name, type, protocol, timeout, endpoint_id, collector_ids, collector_tags, enabled, frequency, health_steps, health_collectors, health_notification_enabled, health_notification_email) do
     settings = [
       %{variable: "name", value: name},
       %{variable: "type", value: type},
